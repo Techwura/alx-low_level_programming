366248ec9189da4b2f5712a973859408309f656e
Afia <afiaserwaaamponsem@gmail.com>
2023-10-25 21:40:03 -0700

    5-sqrt_recursion.c file

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,15 @@
 nclude "main.h"
 /**
  * _pow_recursion - function name
- * @b: parameter 1
- * @c: parameter 2
+ * @x: parameter 1
+ * @y: parameter 2
  * Return: value of a given number passed to the function
  */
-int _pow_recursion(int b, int c)
+int _pow_recursion(int x, int y)
 {
-	if (c < 0)
+	if (y < 0)
 		return (-1);
-	if (c == 0)
+	if (y == 0)
 		return (1);
-	return (b * _pow_recursion(b, c - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
