d59ee375e2777d42b83d36361cda19cf6a06ca5e
Afia <afiaserwaaamponsem@gmail.com>
2023-11-02 22:30:29 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - cause normal process termination with a status value of 98
+ *
+ * @b: allocated memory
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *u;
+
+	u = malloc(b);
+	if (u == NULL)
+		exit(98);
+	return (u);
+}
