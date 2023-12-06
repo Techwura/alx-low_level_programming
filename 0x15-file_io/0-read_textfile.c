f3d4422981a12cc46d73f15f9c6fd90757ca7996
Techwura <afiaserwaaamponsem@gmail.com>
2023-12-05 16:47:52 -0800

    file i/o proj

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *labai;
+	ssize_t lorinda;
+	ssize_t vee;
+	ssize_t land;
+
+	lorinda = open(filename, O_RDONLY);
+	if (lorinda == -1)
+		return (0);
+	labai = malloc(sizeof(char) * letters);
+	land = read(lorinda, labai, letters);
+	vee = write(STDOUT_FILENO, labai, land;
+
+	free(labai);
+	close(lorinda);
+	return (vee);
+}
