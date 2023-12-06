702d2fc29ba37d0b509e09a2531943c64c5ef172
Techwura <afiaserwaaamponsem@gmail.com>
2023-12-05 16:57:04 -0800

    file i/o proj

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,9 +20,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	labai = malloc(sizeof(char) * letters);
 	land = read(lorinda, labai, letters);
-	vee = write(STDOUT_FILENO, labai, land;
+	vee = write(STDOUT_FILENO, labai, land);
 
 	free(labai);
 	close(lorinda);
-	return (vee);
+	return vee;
 }
