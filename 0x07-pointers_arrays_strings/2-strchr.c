3725c504380a44c21e6af8d59a5ed0bdab21edd5
Techwura <afiaserwaaamponsem@gmail.com>
2024-01-01 11:13:55 -0800

    pointers and arrays

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,11 +5,11 @@
  * @c: input
  * Return: Always 0 (Success)
  */
-char *_strchr(char *s, cahr c)
+char *_strchr(char *s, char c)
 {
 	int ada = 0;
 
-	for (; s[ada] >= '\0', ada++)
+	for (; s[ada] >= '\0'; ada++)
 	{
 		if (s[ada] == c)
 			return (&s[ada]);
